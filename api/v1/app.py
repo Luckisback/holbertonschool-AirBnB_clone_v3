6df112704aab35afe5f331e58ddb2c519522efaf
saima-riaz <saimariaz1422@gmail.com>
2024-01-03 02:31:44 -0800

    modify app.py file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
-from flask import Flask, jsonify, make_response, Blueprint
+"""Initializes the Flask application"""
+
 from models import storage
 from api.v1.views import app_views
-from os import environ
-from flasgger import Swagger
+from os import getenv
+from flask import Flask, jsonify, Blueprint
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-swagger = Swagger(app)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Close database connection after app context"""
+    """Called to close the storage after each request"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """Handle 404 errors with a JSON response"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def handler_error(stat):
+    """returns a JSON-formatted 404 status code response"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    """Main Function"""
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(environ.get('HBNB_API_PORT', 5000))
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
